c75e1f30f1e2282228d38426d9a90c6abe92e6f7
Ryan Workman <rdavid1099@gmail.com>
2019-01-12 15:54:51 -0800

    Initial commit

---
+++ lib/poke/api/v2.rb
@@ -0,0 +1,9 @@
+require "poke/api/v2/version"
+
+module Poke
+  module Api
+    module V2
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/poke/api/v2/version.rb
@@ -0,0 +1,7 @@
+module Poke
+  module Api
+    module V2
+      VERSION = "0.1.0"
+    end
+  end
+end
