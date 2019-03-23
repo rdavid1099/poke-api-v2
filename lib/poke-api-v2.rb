6310aebf891024ec9e02a9fa034f20281c906b01
Ryan Workman <rdavid1099@gmail.com>
2019-03-23 16:26:40 -0700

    Fix Pokemon object to follow PokeApi namespacing pattern

--- lib/poke-api-v2.rb
+++ lib/poke-api-v2.rb
@@ -14,6 +14,6 @@ module PokeApi
     ErrorHandling.raise_too_many_args_error if id && name
     query = id || name || (rand(POKEMON_COUNT) + 1)
     raw_data = Fetcher.call(:pokemon, query)
-    Pokemon.new(raw_data)
+    PokeApi::Pokemon.new(raw_data)
   end
 end
