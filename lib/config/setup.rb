6f463a2e0626a31eca84ed47f32f48610b74b42b
Ryan Workman <rdavid1099@gmail.com>
2019-03-23 17:57:45 -0700

    Force constants to be loaded after all files are loaded

--- lib/config/setup.rb
+++ lib/config/setup.rb
@@ -3,5 +3,8 @@ require 'json'
 
 path = __dir__
 Dir.glob("#{path}/../**/*.rb").each do |file|
-  require file.sub('.rb', '').to_s unless file.split('/').last == 'setup.rb'
+  setup_or_constants = file.split('/').last == 'setup.rb' || file.split('/').last == 'constants.rb'
+  require file.sub('.rb', '').to_s unless setup_or_constants
 end
+
+require_relative './constants'
