8be0bc12d4fa1af159230f0e453ad8b43c6303fb
Ryan Workman <rdavid1099@gmail.com>
2019-03-26 20:59:15 -0700

    Add objects tied to VersionGroup to VersionGroup object

--- spec/unit/poke_api/version_group_spec.rb
+++ spec/unit/poke_api/version_group_spec.rb
@@ -11,6 +11,10 @@ RSpec.describe PokeApi::VersionGroup, :vcr do
       expect(version_group.name).to eq('red-blue')
       expect(version_group.order).to eq(1)
       expect(version_group.versions.first.class).to eq(PokeApi::Version)
+      # expect(version_group.generation.class).to eq(PokeApi::Generation)
+      expect(version_group.move_learn_methods.first.class).to eq(PokeApi::MoveLearnMethod)
+      expect(version_group.pokedexes.first.class).to eq(PokeApi::Pokedex)
+      expect(version_group.regions.first.class).to eq(PokeApi::Region)
     end
   end
 end
