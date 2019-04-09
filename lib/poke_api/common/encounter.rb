a1a491f1c3ff4982bf59be6fc6da748e9361acbb
Ryan Workman <rdavid1099@gmail.com>
2019-04-08 20:33:30 -0700

    Uncomment references to EncounterConditionValue

--- lib/poke_api/common/encounter.rb
+++ lib/poke_api/common/encounter.rb
@@ -2,6 +2,8 @@ module PokeApi
   module Common
     # Encounter object handling lists of EncounterMethod and EncounterConditionValue
     class Encounter
+      include AssignmentHelpers
+
       attr_reader :chance,
                   :condition_values,
                   :max_level,
@@ -10,7 +12,10 @@ module PokeApi
 
       def initialize(data)
         @chance = data[:chance]
-        # @condition_values = PokeApi::EncounterConditionValue.new(data[:condition_values])
+        @condition_values = assign_list(
+          data: data[:condition_values],
+          klass: PokeApi::EncounterConditionValue
+        )
         @max_level = data[:max_level]
         @method = PokeApi::EncounterMethod.new(data[:method])
         @min_level = data[:min_level]
