979884e0f6636364d6c82ddebb0a64b90d7ef442
Ryan Workman <rdavid1099@gmail.com>
2019-04-26 17:50:02 -0700

    Add Nature::NatureStatChange and Nature::MoveBattleStylePreference objects and tests

---
+++ spec/unit/poke_api/nature/move_battle_style_preference_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe PokeApi::Nature::MoveBattleStylePreference do
+  describe '#initialize' do
+    it 'creates a basic MoveBattleStylePreference object from raw json data' do
+      raw_data = {
+        low_hp_preference: 32,
+        high_hp_preference: 30,
+        move_battle_style: {
+          name: "attack",
+          url: "https://pokeapi.co/api/v2/move-battle-style/1/"
+        }
+      }
+      mbsp = PokeApi::Nature::MoveBattleStylePreference.new(raw_data)
+
+      expect(mbsp.class).to eq(PokeApi::Nature::MoveBattleStylePreference)
+      expect(mbsp.low_hp_preference).to eq(32)
+      expect(mbsp.high_hp_preference).to eq(30)
+      # expect(mbsp.move_battle_style.class).to eq(PokeApi::MoveBattleStyle)
+    end
+  end
+end
