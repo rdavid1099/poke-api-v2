ff7eb34f3478b48ad4735faf09bf22b48378544f
Ryan Workman <rdavid1099@gmail.com>
2019-05-02 12:46:27 -0700

    Add PokemonShape::AwesomeName object and test

--- lib/poke_api/pokemon_shape.rb
+++ lib/poke_api/pokemon_shape.rb
@@ -8,5 +8,11 @@ module PokeApi
     def initialize(data)
       assign_data(data)
     end
+
+    private
+
+    def custom_endpoint_object
+      { awesome_names: AwesomeName }
+    end
   end
 end
