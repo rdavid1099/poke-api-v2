a9b38ca2db62ecde553340bf1dc1b138c5c48817
Ryan Workman <rdavid1099@gmail.com>
2019-05-03 12:18:16 -0700

    Add EggGroup test

--- spec/unit/poke_api/egg_group_spec.rb
+++ spec/unit/poke_api/egg_group_spec.rb
@@ -1,14 +1,14 @@
 RSpec.describe PokeApi::EggGroup, :vcr do
   describe '#initialize' do
     it 'creates a EggGroup object from raw json data' do
-      raw_data = Fetcher.call(:pokemon_color, 1)
-      pokemon_color = PokeApi::EggGroup.new(raw_data)
+      raw_data = Fetcher.call(:egg_group, 1)
+      egg_group = PokeApi::EggGroup.new(raw_data)
 
-      expect(pokemon_color.class).to eq(PokeApi::EggGroup)
-      expect(pokemon_color.id).to eq(1)
-      expect(pokemon_color.name).to eq('black')
-      expect(pokemon_color.names.first.class).to eq(PokeApi::Common::Name)
-      expect(pokemon_color.pokemon_species.first.class).to eq(PokeApi::PokemonSpecies)
+      expect(egg_group.class).to eq(PokeApi::EggGroup)
+      expect(egg_group.id).to eq(1)
+      expect(egg_group.name).to eq('monster')
+      expect(egg_group.names.first.class).to eq(PokeApi::Common::Name)
+      expect(egg_group.pokemon_species.first.class).to eq(PokeApi::PokemonSpecies)
     end
   end
 end
