87c7de7f48c340de6bde1dd80b3d37380c3d7366
Ryan Workman <rdavid1099@gmail.com>
2019-05-03 21:54:11 -0700

    Refactor Fetcher class methods to be in self block

--- lib/utils/fetcher.rb
+++ lib/utils/fetcher.rb
@@ -1,19 +1,21 @@
 # Simple object to handle all data fetching and parsing
 class Fetcher
-  def self.call(endpoint, query = nil)
-    path = "#{BASE_URI}#{ENDPOINTS[endpoint]}#{sanitize_query(query)}"
-    call_uri(path).merge(resource_name: endpoint)
-  end
+  class << self
+    def call(endpoint, query = nil)
+      path = "#{BASE_URI}#{ENDPOINTS[endpoint]}#{sanitize_query(query)}"
+      call_uri(path).merge(resource_name: endpoint)
+    end
 
-  def self.call_uri(path)
-    uri  = URI(path)
-    resp = Net::HTTP.get(uri)
-    JSON.parse(resp, symbolize_names: true).merge(url: path)
-  end
+    def call_uri(path)
+      uri  = URI(path)
+      resp = Net::HTTP.get(uri)
+      JSON.parse(resp, symbolize_names: true).merge(url: path)
+    end
 
-  def self.sanitize_query(query)
-    return query unless query.is_a? Hash
+    def sanitize_query(query)
+      return query unless query.is_a? Hash
 
-    query.reduce('?') { |result, param| result + "#{param[0]}=#{param[1]}&" }.chop
+      query.reduce('?') { |result, param| result + "#{param[0]}=#{param[1]}&" }.chop
+    end
   end
 end
